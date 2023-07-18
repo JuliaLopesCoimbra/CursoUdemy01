5848f97dc1a4f846ced0d506883b50331d7499aa
Júlia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2023-07-18 20:22:06 -0300

    aula15

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-var area = document.getElementById("area")
-
-function Entrar(){
-    var nome = prompt("Digite seu nome");
-    if(nome == "" || nome == null){
-        alert("ops não há nada")
-        area.innerHTML = "CLIQUE NO BOTÃO PARA ACESSAR..."
-    }else{
-    area.innerHTML = "Bem vindo " + nome;
-    
-    let BotaoSair = document.createElement("button");
-    BotaoSair.innerHTML = "Sair da Conta";
-    area.appendChild(BotaoSair);
-    BotaoSair.onclick = sair;
-    }
-
+function acao(){
+    document.write("Executando....")
 }
-function sair(){
-    alert("até mais !")
-    area.innerHTML = "Vc saiu."
-
-}
-
-//testar no console
-function arrays(){
-    var lista = ["Mat", "José", 150]
-    //lista.length = digite no console e veja o tamanho da lista
-    //lista[0] = irá retornar Mat pq é o primeiro item da lista
-}
+setInterval(acao, 1000)//setInterval é uma função que repete em intervalo, nesse caso, a funtion acao irá repetir de 1 segundo
