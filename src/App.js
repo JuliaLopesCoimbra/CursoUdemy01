86a8efdd5c7bcf5b779475b23b51f9c2e635ebc3
Júlia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2023-07-25 19:23:56 -0300

    aula26PropsEComponentes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Nome from './components/Nome';
+function App(){
+  return(
+    <div>
+      <h1>Componente App</h1>
+      <Nome aluno ="Julia" idade={25}/>
+    </div>
+  );
+}
+export default App;
