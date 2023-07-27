754f8c6b4f53c93025009091943dbb6f66222271
Júlia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2023-07-27 20:34:05 -0300

    Aula30

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import Nome from './components/Nome';
+import { useState } from 'react';
+
 function App(){
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
+  const [email, setEmail] = useState('');
+  const[user, setUser] = useState({});
+
+  function handleRegister(e){
+    e.preventDefault();
+    alert("Usuário Cadastrado com Sucesso!")
+    setUser({
+      nome: nome,
+      idade: idade, 
+      email: email,
+    })
+  }
   return(
     <div>
-      <h1>Componente App</h1>
-      <Nome aluno ="Julia" idade={25}/>
+      <h1>Cadastrando Usuario</h1>
+      <form onSubmit={handleRegister}>
+
+        <label>Nome:</label><br></br>
+        <input
+        placeholder='Digite seu nome:'
+        value={nome}
+        onChange={(e)=> setNome(e.target.value)}
+        ></input><br></br>
+
+        <label>Idade:</label><br></br>
+        <input
+        placeholder='Digite sua idade:'
+        value={idade}
+        onChange={(e)=> setIdade(e.target.value)}
+        ></input><br></br>
+
+        <label>Email:</label><br></br>
+        <input
+        placeholder='Digite seu email:'
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
+        ></input><br></br>
+
+        <button type='submit'>Registrar</button>
+      </form>
+      <br></br>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span>
+        <span>Sua idade: {user.idade}</span>
+        <span>Seu email: {user.email}</span>
+      </div>
     </div>
   );
 }
